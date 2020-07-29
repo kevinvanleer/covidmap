124c86b69ed6e661aedaf5e377fac19eac2ec9be
Kevin Van Leer <kevin.vanleer@gmail.com>
2020-07-29 08:48:31 -0500

    Cleanup

--- src/components/core/Map.js
+++ src/components/core/Map.js
@@ -16,8 +16,6 @@ const MapboxMap = ({ activeLayers, layers, ...props }) => {
   const [map, setMap] = useState(null);
   const mapContainer = useRef(null);
 
-  console.debug('rendering');
-
   useEffect(() => {
     const dataPromise = fetchUsCasesByCounty();
     const map = new mapboxgl.Map({
@@ -28,7 +26,6 @@ const MapboxMap = ({ activeLayers, layers, ...props }) => {
     });
     setMap(map);
     map.on('load', () => {
-      console.debug('map loaded');
       map.addSource('us-counties', {
         type: 'geojson',
         data: '/api/us-geo-cases-by-county',
@@ -37,67 +34,7 @@ const MapboxMap = ({ activeLayers, layers, ...props }) => {
         type: 'geojson',
         data: '/api/us-county-centroids',
       });
-      /*map.addLayer({
-        id: 'us-county-total-deaths',
-        type: 'fill',
-        source: 'us-counties',
-        paint: {
-          'fill-color': '#f00',
-          'fill-opacity': [
-            'interpolate',
-            ['linear'],
-            ['feature-state', 'deaths'],
-            0,
-            0,
-            1,
-            0.1,
-            10,
-            0.2,
-            100,
-            0.5,
-            1000,
-            0.9,
-            2000,
-            1,
-          ],
-        },
-      });
-      map.addLayer({
-        id: 'us-county-total-cases',
-        type: 'fill',
-        source: 'us-counties',
-        paint: {
-          'fill-color': '#00f',
-          'fill-opacity': [
-            'interpolate',
-            ['linear'],
-            ['feature-state', 'cases'],
-            0,
-            0,
-            10,
-            0.1,
-            100,
-            0.2,
-            10000,
-            0.5,
-            100000,
-            0.9,
-            200000,
-            1,
-          ],
-        },
-      });
-      map.addLayer({
-        id: 'us-county-centroids',
-        type: 'symbol',
-        source: 'us-county-centroids',
-        minzoom: 7,
-        layout: { 'text-field': ['get', 'NAME'] },
-        paint: {
-          'text-halo-color': '#fff',
-          'text-halo-width': 0.5,
-        },
-      });*/
+
       layers.forEach((layer) => map.addLayer(layer));
 
       layers.forEach((layer) =>
