14f9e48546baef829a847f8a568b51a523a3c229
Kevin Van Leer <kevin.vanleer@gmail.com>
2020-08-12 08:36:51 -0500

    Updates

---
+++ precache-manifest.dbcff52de1c01044c6b530e7dd14263c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "199dd53e7b73f32ccc6aaefb03d2fb55",
+    "url": "/index.html"
+  },
+  {
+    "revision": "69821498a28a1d4ff02c",
+    "url": "/static/css/2.ed590494.chunk.css"
+  },
+  {
+    "revision": "5368c3355bde7548e234",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "69821498a28a1d4ff02c",
+    "url": "/static/js/2.f5edfe67.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/static/js/2.f5edfe67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5368c3355bde7548e234",
+    "url": "/static/js/main.e97bc547.chunk.js"
+  },
+  {
+    "revision": "b5304dd74a00a4a66497",
+    "url": "/static/js/runtime-main.f924a015.js"
+  }
+]);
