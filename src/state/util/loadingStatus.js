c7c55544fb6e7db736fcd20c9a3eeda98150e943
Kevin Van Leer <kevin.vanleer@gmail.com>
2020-08-14 07:28:57 -0500

    Added loading status utilities

---
+++ src/state/util/loadingStatus.js
@@ -0,0 +1,12 @@
+export const loadingStatus = {
+  pending: 'LOADING_PENDING',
+  loading: 'LOADING_LOADING',
+  complete: 'LOADING_COMPLETE',
+};
+
+export const loadStatusDefaults = {
+  status: null,
+  success: null,
+  error: null,
+  result: null,
+};
