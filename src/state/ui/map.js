b15bae49f1b4744e3a325a4524c55fdc7596ecde
Kevin Van Leer <kevin.vanleer@gmail.com>
2020-08-14 07:29:17 -0500

    Added state for map object and loading status

--- src/state/ui/map.js
+++ src/state/ui/map.js
@@ -1,8 +1,16 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { loadingStatus, loadStatusDefaults } from '../util/loadingStatus.js';
 
 const mapSlice = createSlice({
   name: 'map',
-  initialState: { hold: false, selectedFeature: null },
+  initialState: {
+    hold: false,
+    selectedFeature: null,
+    mapLoadStatus: { ...loadStatusDefaults },
+    sourcesLoadStatus: { ...loadStatusDefaults },
+    layersLoadStatus: { ...loadStatusDefaults },
+    map: null,
+  },
   reducers: {
     hold: (state) => {
       state.hold = true;
@@ -16,6 +24,30 @@ const mapSlice = createSlice({
     setSelectedFeature: (state, action) => {
       state.selectedFeature = action.payload;
     },
+    beginMapInitialization: (state) => {
+      state.mapLoadStatus.status = loadingStatus.pending;
+      state.sourcesLoadStatus.status = loadingStatus.pending;
+      state.layersLoadStatus.status = loadingStatus.pending;
+    },
+    beginLoadingMap: (state, action) => {
+      state.map = action.payload;
+      state.mapLoadStatus.status = loadingStatus.loading;
+    },
+    mapFinishedLoading: (state) => {
+      state.mapLoadStatus.status = loadingStatus.complete;
+    },
+    beginLoadingSources: (state) => {
+      state.sourcesLoadStatus.status = loadingStatus.loading;
+    },
+    sourcesFinishedLoading: (state) => {
+      state.sourcesLoadStatus.status = loadingStatus.complete;
+    },
+    beginLoadingLayers: (state) => {
+      state.layersLoadStatus.status = loadingStatus.loading;
+    },
+    layersFinishedLoading: (state) => {
+      state.layersLoadStatus.status = loadingStatus.complete;
+    },
   },
 });
 
@@ -24,5 +56,12 @@ export const {
   hold,
   releaseHold,
   setSelectedFeature,
+  beginMapInitialization,
+  beginLoadingMap,
+  mapFinishedLoading,
+  beginLoadingSources,
+  sourcesFinishedLoading,
+  beingLoadingLayers,
+  layersFinishedLoading,
 } = mapSlice.actions;
 export default mapSlice.reducer;
