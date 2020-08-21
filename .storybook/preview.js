3be2fb0c160674c1ef6217f7741ffcb1cb99f6f6
Kevin Van Leer <kevin.vanleer@gmail.com>
2020-08-21 12:15:03 -0500

    Upgraded storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -12,5 +12,3 @@ const req = require.context('../src', true, /\.stories\.js$/);
 const loadStories = () => {
   req.keys().forEach((filename) => req(filename));
 };
-
-configure(loadStories, module);
