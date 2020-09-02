623dfc27509e584e65dd37cdf270031551070a15
Kevin Van Leer <kevin.vanleer@gmail.com>
2020-09-02 09:30:07 -0500

    Disable for non-prod builds

--- src/AnalyticsProvider.js
+++ src/AnalyticsProvider.js
@@ -3,6 +3,7 @@ import { MatomoProvider, createInstance } from '@datapunt/matomo-tracker-react';
 
 const matomoInstance = createInstance({
   urlBase: 'https://kevinvanleer.matomo.cloud/',
+  disabled: process.env.NODE_ENV !== 'production', // optional, false by default. Makes all tracking calls no-ops if set to true.
 });
 
 export const AnalyticsProvider = ({ ...props }) => (
